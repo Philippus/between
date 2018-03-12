839419559dedfdb36fc7009ab23d3a118923d5fd
Philippus Baalman <philippus@gmail.com>
2018-03-12 21:09:38 +0100

    Add example based tests for constraints method

--- src/test/scala/nl/gn0s1s/between/RelationSpec.scala
+++ src/test/scala/nl/gn0s1s/between/RelationSpec.scala
@@ -13,4 +13,19 @@ object RelationSpec extends Properties("Relation") {
     relation =>
       relation.inverse.inverse.equals(relation)
   }
+
+  property("from the paper - constraints between L and R given path: R <-(< m mi >)- - S - -(o m)-> L ") =
+    Relation.constraints(Set(oi, mi), Set(<, m, mi, >)) == Set(<, >, o, m, di, s, si, fi, is)
+
+  property("from the paper - constraints between S and R given path: S - -(o m)-> L - - (o s)-> R") =
+    Relation.constraints(Set(o, m), Set(o, s)) == Set(<, o, m)
+
+  property("from the paper - constraints between D and R given path: D - -(d)-> S - -(< m)-> R") =
+    Relation.constraints(Set(d), Set(<, m)) == Set(<)
+
+  property("from the paper - constraints between D and L given path: D - -(d)-> S - -(o m)-> L") =
+    Relation.constraints(Set(d), Set(o, m)) == Set(<, o, m, d, s)
+
+  property("from the readme - constraints between W and D given path: W <-(o)- - T - -(>, mi)-> D") =
+    Relation.constraints(Set(o), Set(<, m)) == Set(<)
 }
